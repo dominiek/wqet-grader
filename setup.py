b0ee29af9725247b42c396262638c79d6ad88918
Dominiek Ter Heide <info@dominiek.com>
2022-01-12 20:47:08 -0500

    Tweaks

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ class install(_install):
 
 setup(
     name="wqet_grader",
-    version="0.1.17",
+    version="0.1.18",
     description='Grading for WQET',
     url='https://github.com/dominiek/wqet-grader',
     cmdclass={'install': install},
