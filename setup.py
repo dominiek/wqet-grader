7ed01e490893198c226801be180459333ec3cce7
Dominiek Ter Heide <info@dominiek.com>
2022-02-07 18:48:05 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ class install(_install):
 
 setup(
     name="wqet_grader",
-    version="0.1.18",
+    version="0.1.19",
     description='Grading for WQET',
     url='https://github.com/dominiek/wqet-grader',
     cmdclass={'install': install},
