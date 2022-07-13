0b96bdd4ecdb42343f95a2df8779457a3255ae6a
ncgoodbody <nicholas.goodbody@gmail.com>
2022-07-13 16:34:09 -0700

    feat: Add bson cleaner

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ class install(_install):
 
 setup(
     name="wqet_grader",
-    version="0.1.22",
+    version="0.1.23",
     description='Grading for WQET',
     url='https://github.com/dominiek/wqet-grader',
     cmdclass={'install': install},
